859237ba774dbf0ed72c92ca148612af316340d3
JabirKhalil <jr7963@gmail.com>
2022-01-09 17:00:16 +0100

    Add project files.

---
+++ WebSecurity/Migrations/20211226170629_Inits.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebSecurity.Migrations
+{
+    public partial class Inits : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ WebSecurity/Models/Comment.cs
@@ -0,0 +1,9 @@
+﻿namespace WebSecurity.Models
+{
+    public class Comment
+    {
+        public Guid Id { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string? Content { get; set; }
+    }
+}
